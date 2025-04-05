6186a6e81bf19a3141b39e2290c0ee0986657bc1
SArminkaS <arminka4320@outlook.hu>
2025-04-05 17:57:30 +0200

    change from jwt to basic authentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,14 +2,17 @@
 import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './auth.dto';
+import { UserService } from 'src/user/user.service';
+import { User } from 'src/models/user.model';
 
 @Controller('auth')
 export class AuthController {
-constructor(private readonly authService: AuthService) {}
+constructor(
+private readonly userService: UserService) {}
 
-  @HttpCode(HttpStatus.OK)
-  @Post('login')
-  signIn(@Body() user: AuthDto) {
-    return this.authService.signIn(user.username, user.password);
+  @Post('register')
+  register(@Body() user: AuthDto) {
+    const new_user = User.build({username: user.username, password: user.password})
+    return this.userService.addOne(new_user)
   }
 }
--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,41 +1,24 @@
+import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
-import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-  import { JwtService } from '@nestjs/jwt';
-  import { Request } from 'express';
-import { AuthDto } from './auth.dto';
-  
-  @Injectable()
-  export class AuthGuard implements CanActivate {
-    constructor(
-        private readonly jwtService: JwtService,
-        private readonly configService: ConfigService) {}
-  
-    async canActivate(context: ExecutionContext) {
-      const request = context.switchToHttp().getRequest();
-      const token = this.extractTokenFromHeader(request);
-      if (!token) {
-        throw new UnauthorizedException('Jelentkezz be!');
-      }
-      try {
-        const payload = await this.jwtService.verifyAsync(
-          token,
-          {
-            secret: this.configService.get<string>('JWT_SECRETKEY')
-          }
-        );
-        const user = new AuthDto()
-        user.password = payload['password']
-        user.username = payload['username']
-        request['user'] = user;
-      } catch {
-        throw new UnauthorizedException();
-      }
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private readonly authService: AuthService){}
+  async canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest();
+    const authHeader = request.headers['authorization'];
+
+    if (!authHeader || !authHeader.startsWith('Basic ')) {
+      throw new UnauthorizedException('A felhasználónév vagy jelszó hiányzik vagy nem megfelelően lett megadva!');
+    }
+
+    const base64Credentials = authHeader.split(' ')[1];
+    const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
+    const [username, password] = credentials.split(':');
+    const user = await this.authService.authUser(username, password)
+    if (username === user.username && password === user.password) {
       return true;
     }
-  
-    private extractTokenFromHeader(request: Request) {
-      const [type, token] = request.headers.authorization?.split(' ') ?? [];
-      return type === 'Bearer' ? token : undefined;
-    }
-  }  
+      throw new UnauthorizedException('Hibás jelszó vagy felhasználónév!')
+  }
+}

0a6ff2b3f0488dd004b5f274b63f4189a5c71131
SArminkaS <arminka4320@outlook.hu>
2025-04-05 14:44:50 +0200

    implement auth and jwt token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+
+import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './auth.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+constructor(private readonly authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  signIn(@Body() user: AuthDto) {
+    return this.authService.signIn(user.username, user.password);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+
+import { HttpException, HttpStatus, Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService
+  ) {}
+
+  async signIn(username: string, password: string) {
+    const user = await this.userService.getOne(username)
+    if(user == null)
+    {
+      throw new HttpException('A felhasználó nem található!', HttpStatus.NOT_FOUND)
+    }
+    else if(user.password !== password)
+    {
+      throw new UnauthorizedException('Rossz jelszó!');
+    }
+    const payload = { username: user.username }
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    }
+  }
+}

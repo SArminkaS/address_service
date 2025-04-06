bfe9b88dc46b19ed479fe053dae01cc7b15a4cd7
SArminkaS <arminka4320@outlook.hu>
2025-04-06 18:36:05 +0200

    fix main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SequelizeValidationFilter } from './exception-filters/validation-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
-import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.TCP,
-      options: {host: '127.0.0.1', port:3001}
-    },
-  );
+  const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new SequelizeValidationFilter());
   app.useGlobalPipes(new ValidationPipe({transform:true}));
-  await app.listen();
+  await app.listen(process.env.PORT || 3002);
 }
 bootstrap();
